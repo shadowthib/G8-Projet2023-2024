7b512626e3af95019ceb158e0f7da5827dfa3254
sAkaD0 <132373139+sAkaD0@users.noreply.github.com>
2024-04-16 11:25:40 +0200

    New classes

--- Sensors/Ultrasound.py
+++ Sensors/Ultrasound.py
@@ -1,1 +1,12 @@
+from Sensor import Sensor
+class UltraSound(Sensor):
+    def __init__(self):
+        super().__init__(self)
+        self.distance = 0
 
+    def set_distance(self, distance):
+        self.distance = distance
+        self.notify()
+
+    def get_distance(self):
+        return self.distance
