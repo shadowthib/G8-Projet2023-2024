f747800ebf53dbe0a5d0042623d7d87425adbe7f
shadowthib <thibault.herry@std.heh.be>
2024-04-17 15:11:06 +0200

    Update UltrasoundTest.py

--- UnitTests/TestingSensors/UltrasoundTest.py
+++ UnitTests/TestingSensors/UltrasoundTest.py
@@ -4,18 +4,22 @@ import time
 
 class UltrasoundTest(unittest.TestCase):
 
-    def pin_unit_test(): # check s'il est bien coonecté sur le bon pin
+    def test_unit_sound (self):
+        self.assertEqual(TRIG.value,1)
+        time.sleep(0.00001)
+        self.assertEqual(ECHO.value,1)
+        time.sleep(0.0011)
+        self.assertEqual(TRIG.value,0)
+        self.assertEqual(ECHO.value,0)
 
-    def type_unit_test(): #check si le type renvoyé est bien un float
+    def test_unit_distance(self):
+        self.assertTrue(0<=distance<=400)
 
-    def distance_unit_test(): # ckeck si la distance renvoyé est bien comprise entre 0 et 400cm
+    def test_unit_type(self):
+        self.assertIsInstance(distance,float)
 
-    def stop_unit_test(): # arreter le moteur si la distence renvoyé est inférieurs à 2cm
-
-    def tourner_unit_test(): # tourner et vérifier qu'il tourne s'il y moins de 10cm sur un capteur
-
-
-
-
+    def test_unit_stop(self):
+        self.assertTrue(distance>5)
+    
 if __name__ == '__main__':
     unittest.main()
