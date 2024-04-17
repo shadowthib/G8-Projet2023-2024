65001519afd4979ddf24f84af68f2c0ecd565b52
Yionidas <yassine.nc23@gmail.com>
2024-04-17 10:12:39 +0200

    Test init

--- UnitTests/TestingSensors/RGBTest.py
+++ UnitTests/TestingSensors/RGBTest.py
@@ -1,10 +1,31 @@
 import unittest
+import board
+import busio
+import adafruit_tcs34725
+import time
 
+class TestTCS34725(unittest.TestCase):
+    def test_CreationTest(self):
+        self.i2c = busio.I2C(1,0)
+        self.sensor = adafruit_tcs34725.TCS34725(self.i2c, address=0x14)
+        self.assertIsNotNone(self.sensor, "Le capteur n'a pas été créé avec succès.")
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_ConnexionTest(self):
+        self.i2c = busio.I2C(1, 0)
+        self.sensor = adafruit_tcs34725.TCS34725(self.i2c, address=0x14)
+        self.assertTrue(self.sensor.active)
+        print("Connexion établie !")
 
+    def test_ReadColor(self):
+        self.i2c = busio.I2C(1, 0)
+        self.sensor = adafruit_tcs34725.TCS34725(self.i2c, address=0x14)
+        if self.sensor.active:
+            self.assertEqual(self.sensor.color_rgb_bytes[0], (200,255), "Couleur ROUGE détectée")
+            self.assertEqual(self.sensor.color_rgb_bytes[1], (200,255), "Couleur VERT détectée")
+            self.assertEqual(self.sensor.color_rgb_bytes[2], (200,255), "Couleur BLEU détectée")
+        else:
+            print("Connexion non établie")
 
 if __name__ == '__main__':
     unittest.main()
+
