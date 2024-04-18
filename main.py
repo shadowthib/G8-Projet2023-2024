248fba0995ff15690a6d91c18d1ffd16d90aad6d
mankouuu <mankoulani.fofanah@std.heh.be>
2024-04-18 13:27:45 +0200

    menu

--- main.py
+++ main.py
@@ -1,18 +1,46 @@
 import threading
 import time
-import PCA9685 as PCA
-import Sensors.Ultrasound as UltraSound
 import Engines.DCEngine as DCEngine
-import Engines.ServoEngine as ServoEngine
+import Sensors.Infrared as InfraRed
+import Sensors.Ultrasound as UltraSound
+import Sensors.RGB as RGB
+
 
 import abc
 
-
 class Main():
     def main(self):
-        print("Main Thread")
+        infra = InfraRed()
+        DC1 =  DCEngine(1, 2, 3)
+        DC2 =  DCEngine(1, 2, 3)
 
 
 
-main = Main()
-main.main()
+    def menu(self):
+        print("1: avancer/reculer 30cm")
+        print("2: gerer accélération")
+        print("3: accelerer/decélération")
+        print("4: tourner servo")
+        print("5: récup donner ultrason")
+        print("6: ")
+
+        return input("Entrez votre choix : ")
+
+while 1:
+    choix=menu()
+    if choix=="1":
+        avancerReculer30cm()
+    elif choix=="2":
+        test_condition()
+    elif choix == "3":
+        accelForward()
+        decelForward()
+    elif choix == "4":
+        test()
+    elif choix == "5":
+        get_distcance()
+    
+   
+        break
+    else:
+        print('Choix non valide, réessayer.')
