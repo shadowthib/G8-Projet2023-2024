3e134d79c9820d68a3cf9e0fefde0df94e945e7a
mankouuu <mankoulani.fofanah@std.heh.be>
2024-04-18 13:25:31 +0200

    menu
    menu

--- main.py
+++ main.py
@@ -13,3 +13,34 @@ class Main():
         infra = InfraRed()
         DC1 =  DCEngine(1, 2, 3)
         DC2 =  DCEngine(1, 2, 3)
+
+
+
+    def menu(self):
+        print("1: avancer/reculer 30cm")
+        print("2: gerer accélération")
+        print("3: accelerer/decélération")
+        print("4: tourner servo")
+        print("5: récup donner ultrason")
+        print("6: ")
+
+        return input("Entrez votre choix : ")
+
+while 1:
+    choix=menu()
+    if choix=="1":
+        avancerReculer30cm()
+    elif choix=="2":
+        test_condition()
+    elif choix == "3":
+        accelForward()
+        decelForward()
+    elif choix == "4":
+        test()
+    elif choix == "5":
+        get_distcance()
+    
+   
+        break
+    else:
+        print('Choix non valide, réessayer.')
