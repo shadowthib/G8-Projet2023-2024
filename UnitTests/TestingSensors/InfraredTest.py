c33c016eaa2e9ca801fcf8bd669a1056906e7b1f
shadowthib <thibault.herry@std.heh.be>
2024-04-18 10:39:58 +0200

    Update InfraredTest.py

--- UnitTests/TestingSensors/InfraredTest.py
+++ UnitTests/TestingSensors/InfraredTest.py
@@ -5,6 +5,19 @@ class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, False)
 
+    def unit_test_exists(self):
+        self.assertIsNotNone(self.sensor)
+    
+    def unit_test_connection(self):
+        self.assertTrue(self.sensor.active)
+    
+    def unit_test_values(self):
+        value = self.sensor.read_value()
+        self.assertIsInstance(value, int, "La valeur renvoyée n'est pas un entier")
+    
+    def unit_test_NonUnderZeroValue(self):
+        value = self.sensor.read_value()
+        self.assertGreaterEqual(value, 0, "La valeur renvoyée est inférieure à 0")
 
 if __name__ == '__main__':
     unittest.main()
