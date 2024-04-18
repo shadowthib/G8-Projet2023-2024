76850301d8a9a8225bb9255577f2f05bad4daf75
Yionidas <yassine.nc23@gmail.com>
2024-04-18 11:51:43 +0200

    Gestion vitesse moteur + obstacle + detection distance ultrason

--- Engines/DCEngine.py
+++ Engines/DCEngine.py
@@ -1,6 +1,9 @@
+import random
+
 import RPi.GPIO as GPIO
 import time
 import PCA9685 as PCA
+import pick
 
 
 class DCEngine:
@@ -14,8 +17,6 @@ class DCEngine:
         self.en_2 = 4
 
         self.pins = [self.motor1_A, self.motor1_B, self.motor2_A, self.motor2_B]
-
-
 
         self.pwm = PCA.PWM()
         self.pwm.frequency = 60
@@ -33,15 +34,20 @@ class DCEngine:
         else:
             self.pwm.write(self.en_2, 0, abs(pwm_value))
 
-    def forward(self):
+    def forward(self, speed_select, direction=None):
         print('Forward')
-        self.motor_state(self.motor1_A, self.motor1_B, 4095)
-        self.motor_state(self.motor2_A, self.motor2_B, 4095)
+        if direction == "R":
+            self.motor_state(self.motor1_A, self.motor1_B, speed_select)
+        elif direction == "L":
+            self.motor_state(self.motor2_A, self.motor2_B, speed_select)
+        else :
+            self.motor_state(self.motor1_A, self.motor1_B, speed_select)
+            self.motor_state(self.motor2_A, self.motor2_B, speed_select)
 
-    def backward(self):
+    def backward(self, speed_select):
         print('Backward')
-        self.motor_state(self.motor1_A, self.motor1_B, -4095)
-        self.motor_state(self.motor2_A, self.motor2_B, -4095)
+        self.motor_state(self.motor1_A, self.motor1_B, -speed_select)
+        self.motor_state(self.motor2_A, self.motor2_B, -speed_select)
 
     def stop(self):
         print('Stop')
@@ -49,24 +55,60 @@ class DCEngine:
         self.motor_state(self.motor2_A, self.motor2_B, 0)
 
 
-
-
-
-def test():
+def test_condition():
 
     motor = DCEngine()
 
+    while True:
+        speed = int(input("Sélectionnez la vitesse : [1] Lent [2] Moyen [3] Rapide [4] Très rapide \n"))
+        if speed >= 1 and speed <= 4:
+            match speed :
+                case 1 :
+                    speed_select = 512
+                    break
+                case 2 :
+                    speed_select = 1028
+                    break
+                case 3 :
+                    speed_select = 2512
+                    break
+                case 4:
+                    speed_select = 4095
+                    break
+        else :
+            print("Inscrivez un nombre entre 1 et 4")
 
-    motor.forward()
 
-    time.sleep(2)
+    while True:
+        obstacle = random.randrange(0, 2)
+        if obstacle == 0:
+            motor.forward(speed_select)
+            time.sleep(1)
+        if obstacle == 1:
+            print("Obstacle détecté !")
+            motor.stop()
+            direction = random.choice(['R', 'L'])
+            motor.forward(speed_select, direction)
+            time.sleep(1)
+            motor.stop()
+            break
+        time.sleep(0.1)
+
+    '''motor.backward(speed_select)
+
+    time.sleep(2)'''
 
 
-    motor.backward()
-    time.sleep(2)
+def test_avance():
+    motor = DCEngine()
+    speed_select = 4095
 
+    while speed_select > 0:
+        motor.forward(speed_select)
+        time.sleep(1)
+        speed_select -= 500
     motor.stop()
 
 
-test()
-
+test_condition()
+#test_avance()
