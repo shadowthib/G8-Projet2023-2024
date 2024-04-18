720f3c53be13d9493a5cce6e2be475cba1f2b127
shadowthib <thibault.herry@std.heh.be>
2024-04-18 11:27:42 +0200

    Update DCEngineTest.py

--- UnitTests/TestingEngines/DCEngineTest.py
+++ UnitTests/TestingEngines/DCEngineTest.py
@@ -1,10 +1,32 @@
 import unittest
+from Engines import DCEngine
 
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, False)  # add assertion here
 
+    def unit_test_exists(self):
+        self.assertIsNotNone(self.engine)
+
+    def unit_test_connection(self):
+        self.assertTrue(self.engine.active)
+
+    
+    def unit_test_vitesse_negative(self):
+        self.motor_state(self.motor1_A, self.motor1_B, -4095)
+        self.motor_state(self.motor2_A, self.motor2_B, -4095)
+        self.assertLess(pwm_value, 0, "La vitesse est négative")
+
+    def unit_test_vitesse_positive(self):
+        self.motor_state(self.motor1_A, self.motor1_B, 4095)
+        self.motor_state(self.motor2_A, self.motor2_B, 4095)
+        self.assertGreater(pwm_value, 0, "La vitesse est positive")
+
+    def unit_test_vitesse_nulle(self):
+        self.motor_state(self.motor1_A, self.motor1_B, 0)
+        self.motor_state(self.motor2_A, self.motor2_B, 0)
+        self.assertEqual(pwm_value, 0, "La vitesse est nulle")
 
 if __name__ == '__main__':
     unittest.main()
