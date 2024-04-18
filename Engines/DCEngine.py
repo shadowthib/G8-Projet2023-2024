b52d8d6fead7ee306d224ede49160a0742b28eed
sAkaD0 <132373139+sAkaD0@users.noreply.github.com>
2024-04-18 09:34:06 +0200

    Speed gestionnary

--- Engines/DCEngine.py
+++ Engines/DCEngine.py
@@ -10,13 +10,15 @@ class DCEngine:
         self.motor2_A = 27
         self.motor2_B = 22
 
-        self.en_1 = 4
-        self.en_2 = 5
+        self.en_1 = 5
+        self.en_2 = 4
 
         self.pins = [self.motor1_A, self.motor1_B, self.motor2_A, self.motor2_B]
 
         self.pwm = PCA.PWM()
         self.pwm.frequency = 60
+
+        self.speed = 4095
 
         GPIO.setmode(GPIO.BCM)
         for pin in self.pins:
@@ -24,16 +26,16 @@ class DCEngine:
 
     def motor_state(self, motorA, motorB, pwm_value):
         GPIO.output(motorA , GPIO.HIGH if pwm_value > 0 else GPIO.LOW)
-        GPIO.output(motorB , GPIO.HIGH if pwm_value < 0 else GPIO.LOW)
-        self.pwm.write(self.en_1 if motorA == self.motor1_A else self.en_1, 0, abs(pwm_value))
+        GPIO.output(motorB , GPIO.LOW if pwm_value > 0 else GPIO.HIGH)
+        self.pwm.write(self.en_1 if motorA == self.motor1_A else self.en_2, 0, abs(pwm_value))
 
     def forward(self):
-        self.motor_state(self.motor1_A, self.motor1_B, 4095)
-        self.motor_state(self.motor2_A, self.motor2_B, 4095)
+        self.motor_state(self.motor1_A, self.motor1_B, self.speed)
+        self.motor_state(self.motor2_A, self.motor2_B, self.speed)
 
     def backward(self):
-        self.motor_state(self.motor1_A, self.motor1_B, -4095)
-        self.motor_state(self.motor2_A, self.motor2_B, -4095)
+        self.motor_state(self.motor1_A, self.motor1_B, -self.speed)
+        self.motor_state(self.motor2_A, self.motor2_B, -self.speed)
 
     def stop(self):
         self.motor_state(self.motor1_A, self.motor1_B, 0)
@@ -46,6 +48,8 @@ class DCEngine:
 def test():
 
     motor= DCEngine()
+
+    motor.speed = int(input("Enter a speed:"))
 
     motor.forward()
     time.sleep(2)
